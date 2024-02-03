eef3f0e924d64c125da9b429528d48985ab0805a
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-02-04 00:22:46 +0530

    running a script for frontend.sh

---
+++ loops.sh
@@ -0,0 +1,9 @@
+#while and for loop
+
+#choose while loop if you are using the expression
+#choose for loop while we are using the inputs
+
+#loop is going to execute the set of commands in iterative way
+a=10
+while [ "$a" -gt 0 ]; do
+  echo hello world
